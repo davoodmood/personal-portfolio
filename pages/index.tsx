24a08aea8a52cb7576af8780178a3f95e083e25d
David Mood <davidmood@hotmail.com>
2022-02-21 13:27:49 +0330

    Revised GTM noscript tag placement. Updating content.

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,12 +37,7 @@ const Home: NextPage = () => {
   })
   
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>davidmood.eth</title>
-        <meta name="description" content="This is Davidmood.eth. I Design, Develope, Sing and Code." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
       <noscript>
         <iframe
           src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
@@ -51,85 +46,91 @@ const Home: NextPage = () => {
           style={{ display: 'none', visibility: 'hidden' }}
         />
       </noscript>
-      
-      <main className={styles.main}>
-      
-      <div className="z-10">
-
-        <animated.h1 className={styles.title} style={props}>davidmood.eth</animated.h1>
-        <p className={styles.description}>
-          <code className={styles.code}>Starman Waiting in the Sky. Here for a mug of tea!</code>
-        </p>
-
-        <div className={styles.grid} >
-          <a href="#" 
-            rel="noreferrer"
-            target={'_blank'}
-            className={styles.card}
-          >
-            <h2>Story & Career &rarr;</h2>
-            <p>Practitioner of Typescript, Nodejs, React, ReactNative and Vue. Blockchain programming enthusiast & a learning gopher.</p>
-          </a>
-
-          <a href="#" className={styles.card}>
-            <h2>Education &rarr;</h2>
-            <p>Blockchain programming enthusiast & a curious gopher! B.A. in English Literature. Self Studied Marketing Management.</p>
-          </a>
-
-          <a
-            href="https://github.com/davoodmood"
-            rel="noreferrer"
-            target={'_blank'}
-            className={styles.card}
-          >
-            <h2>Code &rarr;</h2>
-            <p>Practitioner of Solidity, Typescript, Nodejs, React, ReactNative and Vue. Visit my Github, jog around over 12 opensource projects. </p>
-          </a>
-
-          <a
-            href="https://soundcloud.com/electro_magnet"
-            rel="noreferrer"
-            target={'_blank'}
-            className={styles.card}
-          >
-            <h2>Entertainment &rarr;</h2>
-            <p>
-              Lively with the music, painting and games. DC fan. Rock and Roll & Electronics. Listen to my music on SoundCloud. 
-            </p>
-          </a>
-        </div>
-      </div>
+      <Head>
+        <title>davidmood.eth</title>
+        <meta name="description" content="This is Davidmood.eth. I Design, Develope, Sing and Code." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head> 
+      <div className={styles.container}>     
+        <main className={styles.main}>
         
-        <svg style={{ position: 'absolute', width: 0, height: 0 }} className={'z-0'}>
-          <filter id="goo">
-            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
-            <feColorMatrix
-              in="blur"
-              values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7"
-            />
-          </filter>
-        </svg>
-        <div ref={ref} className={styles.hooksMain} onMouseMove={handleMouseMove}>
-          {trail.map((props, index) => (
-            <animated.div key={index} style={{ transform: props.xy.to(trans) }} />
-          ))}
-        </div>
-      </main>
+        <div className="z-10">
 
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
-      
-    </div>
+          <animated.h1 className={styles.title} style={props}>davidmood.eth</animated.h1>
+          <p className={styles.description}>
+            <code className={styles.code}>Starman Waiting in the Sky. Here for a mug of tea!</code>
+          </p>
+
+          <div className={styles.grid} >
+            <a href="https://www.linkedin.com/in/davidmood/" 
+              rel="noreferrer"
+              target={'_blank'}
+              className={styles.card}
+            >
+              <h2>Story & Career &rarr;</h2>
+              <p>My full-name is Davood Hakimi Mood, aka. David Mood. I have a keen interest in art & design, code & marketing.</p>
+            </a>
+
+            <a href="#" className={styles.card}>
+              <h2>Education &rarr;</h2>
+              <p>B.A. in English Literature. Self-educated and experienced Marketer & Programmer. Blockchain enthusiast & gopher!</p>
+            </a>
+
+            <a
+              href="https://github.com/davoodmood"
+              rel="noreferrer"
+              target={'_blank'}
+              className={styles.card}
+            >
+              <h2>Code & Github &rarr;</h2>
+              <p>Practitioner of Solidity, Typescript, Nodejs, React, ReactNative and Vue. Visit my Github, jog around over 12 opensource projects. </p>
+            </a>
+
+            <a
+              href="https://soundcloud.com/electro_magnet"
+              rel="noreferrer"
+              target={'_blank'}
+              className={styles.card}
+            >
+              <h2>Entertainment &rarr;</h2>
+              <p>
+                Lively with the music, painting and games. DC fan. Rock and Roll & Electronics. Listen to my music on SoundCloud. 
+              </p>
+            </a>
+          </div>
+        </div>
+          
+          <svg style={{ position: 'absolute', width: 0, height: 0 }} className={'z-0'}>
+            <filter id="goo">
+              <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="30" />
+              <feColorMatrix
+                in="blur"
+                values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7"
+              />
+            </filter>
+          </svg>
+          <div ref={ref} className={styles.hooksMain} onMouseMove={handleMouseMove}>
+            {trail.map((props, index) => (
+              <animated.div key={index} style={{ transform: props.xy.to(trans) }} />
+            ))}
+          </div>
+        </main>
+
+        {/* <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <span className={styles.logo}>
+              <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            </span>
+          </a>
+        </footer> */}
+        
+      </div>
+    </>
   )
 }
 
